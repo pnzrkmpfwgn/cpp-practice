be0d6430d728772ade69a9c051c0537ac6214c47
pnzrkmpfwgn <kurtknispel9@gmail.com>
2021-05-05 15:28:16 +0300

    Create and Write File

---
+++ Create and Write File.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace::std;
+
+int main(){
+	ofstream MyFile("filename.txt");
+	
+	MyFile << "Files can be tricky, but it is fun enough!";
+	
+	MyFile.close();
+}
